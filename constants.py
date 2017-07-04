406595410b08cfc4c781f87a2167528cfa3e995d
satyajeet <thakurankitsvr@gmail.com>
2017-07-04 19:48:50 +0530

    self_info() and get_user_id is added

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 #global variable files
 APP_ACCESS_TOKEN='ca0ed5456e294f66bc9320ac60904061'
-#instagram api base ul
+#instagram api base
 BASE_URL='https://api.instagram.com/v1/'
---
+++ mian.py
@@ -0,0 +1,3 @@
+#main file initiated
+from self_info import self_info
+self_info()
