bed7922d42dc7132a95683de1a9e47ce6ced8538
satyajeet <thakurankitsvr@gmail.com>
2017-07-06 01:49:56 +0530

    comment added

--- get_user_info.py
+++ get_user_info.py
@@ -1,21 +1,23 @@
 #fuction for user information
-from __future__ import print_function
+from __future__ import print_function   #giv capability of print fuction used in 3.x python
 import requests
 from get_user_id import *
 from constants import *
 import sys
 import time
 
+#Fetch user info from instagram
 def get_user_info(insta_username):
   user_id = get_user_id(insta_username)
   if user_id == None:
     print ('User does not exist!')
     exit()
-
+  #url for instagram user
   request_url = BASE_URL + 'users/%s?access_token=%s' %(user_id, APP_ACCESS_TOKEN)
   print ('GET request url : %s' % (request_url))
   user_info = requests.get(request_url).json()
 
+  #fetching meta data
   if user_info['meta']['code'] == 200:
       if len(user_info['data']):
 
@@ -25,16 +27,18 @@ def get_user_info(insta_username):
           following='No. of people you are following: %s' % (user_info['data']['counts']['follows'])
           no_of_post= 'No. of posts: %s' % (user_info['data']['counts']['media'])
           info_list=[user,id,follower,following,no_of_post]
+
+          #loop for printing all info store in info list
           for x in range(len(info_list)):
               print(info_list[x],end=' ')
               time.sleep(1)
-
 
       else:
           print ('There is no data for this user!')
   else:
       print ('Status code other than 200 received!')
-# take input from user at time of sys argument
+
+# take input from user as sys argument
 if not len(sys.argv[1:]):
     print ("username must be suplied as argument")
 else:
