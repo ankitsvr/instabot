3c962ebb2c62cdf4cbb4de026e2398fe2cabd1cd
satyajeet <thakurankitsvr@gmail.com>
2017-07-06 01:16:05 +0530

    fetching info in custom style

--- get_user_info.py
+++ get_user_info.py
@@ -1,28 +1,33 @@
-#fuction for user information
-import requests
+#import library and python file for supporting fuction
+from __future__ import print_function
 from get_user_id import *
 from constants import *
-import sys
+#def function for user information
 def get_user_info(insta_username):
   user_id = get_user_id(insta_username)
   if user_id == None:
-    print 'User does not exist!'
+    print ('User does not exist!')
     exit()
 
   request_url = BASE_URL + 'users/%s?access_token=%s' %(user_id, APP_ACCESS_TOKEN)
-  print 'GET request url : %s' % (request_url)
+  print ('GET request url : %s' % (request_url))
   user_info = requests.get(request_url).json()
 
   if user_info['meta']['code'] == 200:
       if len(user_info['data']):
-          print 'Username: %s' % (user_info['data']['username'])
-          print 'ID: %s' % (user_info['data']['id'])
-          print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
-          print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
-          print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+          user='Username: %s' % (user_info['data']['username'])
+          id= 'ID: %s' % (user_info['data']['id'])
+          follower= 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+          following='No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+          no_of_post='No. of posts: %s' % (user_info['data']['counts']['media'])
+
+          info_list=(user,id,follower,following,no_of_post)
+          for x in range(len(info_list)):
+              print(info_list(x),end="....")
+
       else:
-          print 'There is no data for this user!'
+          print ('There is no data for this user!')
   else:
-      print 'Status code other than 200 received!'
+      print ('Status code other than 200 received!')
 
 get_user_info("shivam.walia")
