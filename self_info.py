d652f65807e9a69d3fa547e34923390e9fe1166e
satyajeet <thakurankitsvr@gmail.com>
2017-07-05 11:51:55 +0530

    self written

--- self_info.py
+++ self_info.py
@@ -1,20 +1,19 @@
 import requests
 from constants import *
 
-
 def self_info():
-    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
-    user_info = requests.get(request_url).json()
+    request_url=BASE_URL +'users/self/?access_token=%s' %(APP_ACCESS_TOKEN)
+    print 'GET request url :%s' %(request_url)
+    user_info=requests.get(request_url).json()
 
-    if user_info['meta']['code'] == 200:
+    if user_info['meta']['code']==200:
         if len(user_info['data']):
-            print 'Username: %s' % (user_info['data']['username'])
-            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
-            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
-            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+            print "Username: %s" %user_info['data']['username']
+            print "No of follower: %s" % (user_info['data']['count']['followed_by'])
+            print "no of post :%s" %(user_info['data']['count']['media'])
         else:
-            print 'User does not exist!'
-
+            print "user does not exist"
     else:
-        print 'Status code other than 200 received!'
+        print "Not able to access user instagram info"
+
+
